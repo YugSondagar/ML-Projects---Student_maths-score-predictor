8a14735f19823c5c1dd8f4e48b652794c0f64f3d
Yug Sondagar <yug.sondagar11@gmail.com>
2025-07-25 23:27:09 +0530

    Hyperparameter Tunning

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -22,11 +23,18 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {}
         for name,model in models.items():
+            param = params[name]
+            #model.fit(X_train,y_train)
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
